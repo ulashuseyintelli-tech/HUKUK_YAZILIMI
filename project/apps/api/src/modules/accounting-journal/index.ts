585acce12c8c75eb4286c2e90fbdc53913223c28
ulashuseyintelli-tech <ulashuseyintelli@gmail.com>
2026-06-30 03:13:29 +0300

    feat(accounting): add client offset journal source adapter
    Co-authored-by: Ulas Htelli <ulastelli@limagroup.com.tr>

--- project/apps/api/src/modules/accounting-journal/index.ts
+++ project/apps/api/src/modules/accounting-journal/index.ts
@@ -1,3 +1,6 @@
 export * from './accounting-journal.builder';
+export * from './accounting-journal-source-hash';
+export * from './accounting-journal-source.types';
 export * from './accounting-journal.types';
 export * from './accounting-journal.validators';
+export * from './client-offset-journal-source.adapter';
