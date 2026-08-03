cbbbb1a2de11421e229b460fec55bffb5659e828
Ulaş Hüseyin Telli <ulastelli@limagroup.com.tr>
2026-08-03 16:20:36 +0300

    feat(client): wire notification command authority

--- project/apps/api/src/modules/client-notification/client-notification.module.ts
+++ project/apps/api/src/modules/client-notification/client-notification.module.ts
@@ -6,11 +6,24 @@ import { NotificationDispatchController } from "./notification-dispatch.controll
 import { PrismaModule } from "../../prisma/prisma.module";
 import { OfficeModule } from "../office/office.module";
 import { MessageTemplateModule } from "../message-template/message-template.module";
+import { AuditModule } from "../audit/audit.module";
+import { OfficeApprovalModule } from "../office-approval/office-approval.module";
+import { ClientNotificationAuthorityAdapter } from "./client-notification-authority.adapter";
 
 @Module({
-  imports: [PrismaModule, OfficeModule, MessageTemplateModule],
+  imports: [
+    PrismaModule,
+    OfficeModule,
+    MessageTemplateModule,
+    AuditModule,
+    OfficeApprovalModule,
+  ],
   controllers: [ClientNotificationController, NotificationDispatchController],
-  providers: [ClientNotificationService, NotificationDispatcherService],
+  providers: [
+    ClientNotificationService,
+    NotificationDispatcherService,
+    ClientNotificationAuthorityAdapter,
+  ],
   exports: [ClientNotificationService, NotificationDispatcherService],
 })
 export class ClientNotificationModule {}
